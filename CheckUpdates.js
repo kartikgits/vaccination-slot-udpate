f1d1f982706a78ad11682d7da318ce7e5bd2ee74
kartikgits <kartik.kapparwan1@gmail.com>
2021-05-11 18:37:23 +0530

    Initial Commit

---
+++ CheckUpdates.js
@@ -0,0 +1,65 @@
+const axios=require('axios')
+const request=require('request')
+
+class CheckUpdates {
+
+    static getDate() {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = dd + '/' + mm + '/' + yyyy;
+        return today;
+    }
+
+    static processApiResponse(response){
+        //check last and this time change here
+        const parsedResponse = JSON.parse(response)
+        
+        let centersAvailable=[];
+        
+        let centers=parsedResponse.centers
+        for(var i=0;i<centers.length;i++){
+            let sessions=centers[i].sessions
+            for(var j=0;j<sessions.length;j++){
+                if(sessions[j].min_age_limit<45 && sessions[j].available_capacity>0){
+                    centersAvailable.push(centers[i].name)
+                }
+            }
+        }
+
+        if(centersAvailable.length>0){
+            //send to telegram bot
+            let text=encodeURI("Slots available at:\n\n\n"+centersAvailable.join('\r\n\n')+"\n\n\nPlease book slots at accessible locations only. #StaySafe")
+            let telegramSendUrl=`https://api.telegram.org/bot1777568327:AAElaoDDA8SmazbQpFDP1l5vPbJIykjTlks/sendMessage?chat_id=-1001438859776&text=${text}`
+            axios.get(telegramSendUrl)
+                .then(response => {
+                    // console.log(response)
+                })
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }
+
+    static requestApi(){
+        const date = this.getDate()
+        const pincode = '248001'
+        const apiUrl = `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${date}`
+        
+        let self = this;
+
+        request(apiUrl, function (error, response, body) {
+            if(error){
+                console.log(error)
+            }else if(response && response.statusCode===200){
+                self.processApiResponse(body)
+            }
+        });
+        
+    }
+
+}
+
+module.exports = CheckUpdates
---
+++ index.js
@@ -0,0 +1,19 @@
+const cors=require('cors')
+const express=require('express')
+var cron = require('node-cron')
+const CheckUpdates = require('./CheckUpdates.js')
+
+const app=express()
+const port=process.env.PORT || 5000
+app.use(cors())
+
+//running a cron job every 1 minute
+cron.schedule('*/5 * * * *', () => {
+    console.log('running a task 5 minutes')
+    //call function here
+    CheckUpdates.requestApi()
+});
+
+app.listen(port, ()=> {
+    console.log(`App running at ${port}`)
+})
